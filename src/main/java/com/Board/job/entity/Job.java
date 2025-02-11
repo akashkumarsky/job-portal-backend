83cb5b564aa7455365bff07e52714931018da473
akashkumarsky <akash220701@gmail.com>
2025-02-12 01:25:49 +0530

    removing unwanted annotation

--- src/main/java/com/Board/job/entity/Job.java
+++ src/main/java/com/Board/job/entity/Job.java
@@ -1,14 +1,10 @@
 package com.Board.job.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
+
 
 @Entity
-@Data
-@Builder
-@Getter
 @Table(name = "jobs")
 public class Job {
 
@@ -32,8 +28,63 @@ public class Job {
 
 
 // linking to the employer who posted the job
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    public Job() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
