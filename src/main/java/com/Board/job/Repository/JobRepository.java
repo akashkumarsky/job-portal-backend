6a76c20fbcbb1ecd58ed76f20e98a08c5ba58bfa
akashkumarsky <akash220701@gmail.com>
2025-02-11 20:15:48 +0530

    renamed jobRepository to JobRepository

--- src/main/java/com/Board/job/Repository/JobRepository.java
+++ src/main/java/com/Board/job/Repository/JobRepository.java
@@ -3,6 +3,6 @@ package com.Board.job.Repository;
 import com.Board.job.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface jobRepository extends JpaRepository<Job, Long> {
+public interface JobRepository extends JpaRepository<Job, Long> {
 
 }
